150e63198e7dc748d5c2138bff29838b04bf87bb
Sascha Brandt <myeti@mail.uni-paderborn.de>
2019-09-30 10:13:31 +0200

    Use FetchContent to get libglew

--- cmake/FindGLImplementation.cmake
+++ cmake/FindGLImplementation.cmake
@@ -1,4 +1,4 @@
-# Try to find OpenGL and GLEW, or OpenGL ES 2.x. Once done, this will define:
+# Try to find OpenGL or OpenGL ES 2.x. Once done, this will define:
 #
 #   GLIMPLEMENTATION_FOUND - variable which returns the result of the search
 #   GLIMPLEMENTATION_INCLUDE_DIRS - list of include directories
@@ -18,22 +18,22 @@
 # (To distribute this file outside of CMake, substitute the full
 #  License text for the above reference.)
 
+set(OpenGL_GL_PREFERENCE GLVND CACHE INTERNAL "" FORCE)
 find_package(OpenGL QUIET)
-find_package(GLEW QUIET)
 find_package(GLESv2 QUIET)
 
 # Do not use pkg-config here.
 
-if(OPENGL_FOUND AND GLEW_FOUND)
-	set(GLIMPLEMENTATION_DEFINITIONS "LIB_GL" "LIB_GLEW")
-	set(GLIMPLEMENTATION_INCLUDE_DIR ${OPENGL_INCLUDE_DIR} ${GLEW_INCLUDE_DIRS})
-	set(GLIMPLEMENTATION_LIBRARY ${OPENGL_LIBRARIES} ${GLEW_LIBRARIES})
+if(OPENGL_FOUND)
+	set(GLIMPLEMENTATION_DEFINITIONS "LIB_GL")
+	set(GLIMPLEMENTATION_INCLUDE_DIR ${OPENGL_INCLUDE_DIR})
+	set(GLIMPLEMENTATION_LIBRARY ${OPENGL_LIBRARIES})
 elseif(GLESV2_FOUND)
 	set(GLIMPLEMENTATION_DEFINITIONS "LIB_GLESv2")
 	set(GLIMPLEMENTATION_INCLUDE_DIR ${GLESV2_INCLUDE_DIRS})
 	set(GLIMPLEMENTATION_LIBRARY ${GLESV2_LIBRARIES})
 else()
-	message(FATAL_ERROR "Neither OpenGL and GLEW nor OpenGL ES 2.x were found.")
+	message(FATAL_ERROR "Neither OpenGL nor OpenGL ES 2.x were found.")
 endif()
 
 set(GLIMPLEMENTATION_INCLUDE_DIRS ${GLIMPLEMENTATION_INCLUDE_DIR})
